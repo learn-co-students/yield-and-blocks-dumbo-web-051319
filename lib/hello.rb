b38668e31e87e716b60814ae5a5e22520f47792a
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-03 04:16:39 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
-
+def hello_t(array)
+ i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array
+  # Here, we tell our method to return the original array simply by having that array be the last line of the method. Whatever is evaluated last in a method will be the return value for the whole method. 
 end
 
 # call your method here!
+
+ hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
